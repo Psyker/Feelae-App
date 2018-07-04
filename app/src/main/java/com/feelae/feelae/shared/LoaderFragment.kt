92e32c9f1e6b42d08e14d9390fdf11d4f122e6da
Constantin Guidon <constantin.guidon@gmail.com>
2018-07-04 11:46:56 +0200

    [feature] waiting room

--- app/src/main/java/com/feelae/feelae/shared/LoaderFragment.kt
+++ app/src/main/java/com/feelae/feelae/shared/LoaderFragment.kt
@@ -13,13 +13,11 @@ import com.feelae.feelae.R
  * A simple [Fragment] subclass.
  *
  */
-class LoadingFragment : Fragment() {
+class LoaderFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_loading, container, false)
     }
-
-
 }
